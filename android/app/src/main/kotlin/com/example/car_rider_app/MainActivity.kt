343c0539ec867294c84f58cf59108506a0da216a
Rivaan Ranawat <namanrivaan@gmail.com>
2020-12-19 13:18:21 +0530

    starting the project

---
+++ android/app/src/main/kotlin/com/example/car_rider_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.car_rider_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:car_rider_app/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
