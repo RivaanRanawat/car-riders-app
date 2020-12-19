2668669bed164a63be3a4ee7ba4e7a0e89b1821f
Rivaan Ranawat <namanrivaan@gmail.com>
2020-12-19 19:29:46 +0530

    setting up database for android

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
 import 'package:car_rider_app/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import "dart:io";
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // initialise database
+  final FirebaseApp app = await FirebaseApp.configure(
+    name: 'db2',
+    options: Platform.isIOS
+        ? const FirebaseOptions(
+            googleAppID: '1:297855924061:ios:c6de2b69b03a5be8',
+            gcmSenderID: '297855924061',
+            databaseURL: 'https://flutterfire-cd2f7.firebaseio.com',
+          )
+        : FirebaseOptions(
+            googleAppID: '1:4489037899:android:78c852e8560367be92e5ce',
+            apiKey: 'AIzaSyA3QHg_w4Kg8atFI6Z965PN7N5buFN3zzU',
+            databaseURL: 'https://car-rider-app-default-rtdb.firebaseio.com',
+          ),
+  );
+
   runApp(MyApp());
 }
 
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_database/firebase_database.dart';
 import "package:flutter/material.dart";
 
 class HomeScreen extends StatefulWidget {
@@ -10,6 +11,18 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Home Screen")),
+      body: Center(
+        child: MaterialButton(
+          onPressed: () {
+            DatabaseReference databaseReference = FirebaseDatabase.instance.reference().child("test");
+            databaseReference.set("isConnected");
+          },
+          height: 50,
+          minWidth: 300,
+          color: Colors.blue,
+          child: Text("Chek"),
+        ),
+      ),
     );
   }
 }
