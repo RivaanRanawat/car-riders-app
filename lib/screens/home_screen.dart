0f4dd2354fc9fbb14eaf4c834fb41ddc84a53ea3
Rivaan Ranawat <namanrivaan@gmail.com>
2020-12-20 00:08:51 +0530

    padding google maps correctly and designing navigation drawer

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
+import 'package:car_rider_app/styles.dart';
 import 'package:car_rider_app/universal_variables.dart';
 import 'package:car_rider_app/widgets/reusable_divider.dart';
-import 'package:firebase_database/firebase_database.dart';
 import "package:flutter/material.dart";
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:outline_material_icons/outline_material_icons.dart';
@@ -13,8 +13,11 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
+  double searchSheetHeight = 275;
   Completer<GoogleMapController> _controller = Completer();
   GoogleMapController mapController;
+  double mapPadding = 0.0;
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
@@ -22,23 +25,134 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
+      drawer: Container(
+        width: 250,
+        color: Colors.white,
+        child: Drawer(
+          child: ListView(
+            padding: EdgeInsets.all(0),
+            children: [
+              Container(
+                height: 160,
+                child: DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        "assets/images/user_icon.png",
+                        height: 60,
+                        width: 60,
+                      ),
+                      SizedBox(width: 15),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Rivaan Ranawat",
+                            style: TextStyle(
+                                fontSize: 20, fontFamily: "Bolt-Bold"),
+                          ),
+                          SizedBox(height: 5),
+                          Text("View Profile"),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              ReusableDivider(),
+              SizedBox(height: 10),
+              ListTile(
+                leading: Icon(OMIcons.cardGiftcard),
+                title: Text(
+                  "Free Rides",
+                  style: kDrawerItemStyle,
+                ),
+              ),
+              ListTile(
+                leading: Icon(OMIcons.creditCard),
+                title: Text(
+                  "Payments",
+                  style: kDrawerItemStyle,
+                ),
+              ),
+              ListTile(
+                leading: Icon(OMIcons.history),
+                title: Text(
+                  "Trip History",
+                  style: kDrawerItemStyle,
+                ),
+              ),
+              ListTile(
+                leading: Icon(OMIcons.contactSupport),
+                title: Text(
+                  "Support",
+                  style: kDrawerItemStyle,
+                ),
+              ),
+              ListTile(
+                leading: Icon(OMIcons.info),
+                title: Text(
+                  "About",
+                  style: kDrawerItemStyle,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
       body: Stack(
         children: [
           GoogleMap(
+            padding: EdgeInsets.only(bottom: mapPadding),
             initialCameraPosition: _kGooglePlex,
             mapType: MapType.normal,
             myLocationButtonEnabled: true,
             onMapCreated: (GoogleMapController controller) {
               _controller.complete(controller);
               mapController = controller;
+
+              setState(() {
+                mapPadding = 270;
+              });
             },
+          ),
+          // Drawer Button
+          Positioned(
+            top: 44,
+            left: 22,
+            child: GestureDetector(
+              onTap: (){
+                scaffoldKey.currentState.openDrawer();
+              },
+                          child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 5,
+                          spreadRadius: 0.5,
+                          offset: Offset(0.7, 0.7)),
+                    ]),
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 20,
+                  child: Icon(Icons.menu, color: Colors.black),
+                ),
+              ),
+            ),
           ),
           Positioned(
             left: 0,
             right: 0,
             bottom: 0,
             child: Container(
-              height: 300,
+              height: searchSheetHeight,
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(
@@ -54,30 +168,34 @@ class _HomeScreenState extends State<HomeScreen> {
                 ],
               ),
               child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     SizedBox(height: 5),
-                    Text("Glad to see you!", style: TextStyle(fontSize: 10,)),
-                    Text("Where do you wanna go?", style: TextStyle(fontSize: 18,fontFamily: "Bolt-Semibold"),),
+                    Text("Glad to see you!",
+                        style: TextStyle(
+                          fontSize: 10,
+                        )),
+                    Text(
+                      "Where do you wanna go?",
+                      style:
+                          TextStyle(fontSize: 18, fontFamily: "Bolt-Semibold"),
+                    ),
                     SizedBox(height: 20),
                     Container(
                       decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(4),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black12,
-                            blurRadius: 0.5,
-                            spreadRadius: 0.5,
-                            offset: Offset(
-                              0.7,
-                              0.7
-                            ),
-                          )
-                        ]
-                      ),
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(4),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 0.5,
+                              spreadRadius: 0.5,
+                              offset: Offset(0.7, 0.7),
+                            )
+                          ]),
                       // SEARCH PANEL
                       child: Padding(
                         padding: const EdgeInsets.all(12.0),
@@ -95,14 +213,22 @@ class _HomeScreenState extends State<HomeScreen> {
                     // HOME ADDRESS
                     Row(
                       children: [
-                        Icon(OMIcons.home, color: UniversalVariables.colorDimText),
+                        Icon(OMIcons.home,
+                            color: UniversalVariables.colorDimText),
                         SizedBox(width: 12),
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text("Add Home", style: TextStyle(fontFamily: "Bolt-Regular"),),
+                            Text(
+                              "Add Home",
+                              style: TextStyle(fontFamily: "Bolt-Regular"),
+                            ),
                             SizedBox(height: 3),
-                            Text("Your Address", style: TextStyle(fontFamily: "Bolt-Regular" ,fontSize: 11, color: UniversalVariables.colorDimText))
+                            Text("Your Address",
+                                style: TextStyle(
+                                    fontFamily: "Bolt-Regular",
+                                    fontSize: 11,
+                                    color: UniversalVariables.colorDimText))
                           ],
                         )
                       ],
@@ -113,14 +239,22 @@ class _HomeScreenState extends State<HomeScreen> {
                     // WORK ADDRESS
                     Row(
                       children: [
-                        Icon(OMIcons.workOutline, color: UniversalVariables.colorDimText),
+                        Icon(OMIcons.workOutline,
+                            color: UniversalVariables.colorDimText),
                         SizedBox(width: 12),
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text("Add Work", style: TextStyle(fontFamily: "Bolt-Regular"),),
+                            Text(
+                              "Add Work",
+                              style: TextStyle(fontFamily: "Bolt-Regular"),
+                            ),
                             SizedBox(height: 3),
-                            Text("Your Work Address", style: TextStyle(fontFamily: "Bolt-Regular" ,fontSize: 11, color: UniversalVariables.colorDimText))
+                            Text("Your Work Address",
+                                style: TextStyle(
+                                    fontFamily: "Bolt-Regular",
+                                    fontSize: 11,
+                                    color: UniversalVariables.colorDimText))
                           ],
                         )
                       ],
